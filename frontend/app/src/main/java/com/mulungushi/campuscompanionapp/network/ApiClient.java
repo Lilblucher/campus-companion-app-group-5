d3156a25665cc7a8dc3f71a7e048cce9eb1d21af
Lilblucher <lucasclive3@gmail.com>
2026-09-22 10:15:05 +0200

    linked app to cloud server

--- frontend/app/src/main/java/com/mulungushi/campuscompanionapp/network/ApiClient.java
+++ frontend/app/src/main/java/com/mulungushi/campuscompanionapp/network/ApiClient.java
@@ -5,7 +5,7 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ApiClient {
-    private static final String BASE_URL = "http://192.168.240.1:3000/";
+    private static final String BASE_URL = "http://169.58.92.104:3000/";
     private static Retrofit retrofit;
 
     public static Retrofit getClient() {
